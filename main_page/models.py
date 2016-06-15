b74d6b68b74ab659792f3a71da6c991c681b0d6a
Andrey A. But <aaxbut@gmail.com>
2016-06-15 21:39:59 +0500

    изменен способ вывода информации на гл. странице услуги. левая колонка.

--- main_page/models.py
+++ main_page/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.html import format_html
 
 
 class Post(models.Model):
@@ -15,3 +16,40 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class ServicesMain(models.Model):
+    author = models.ForeignKey('auth.User')
+    service_title = models.CharField(max_length=200)
+    service_href = models.CharField(max_length=200, default='')
+    service_is_deploy = models.BooleanField(default=False)
+    service_column_rl = models.BooleanField(default=False)
+    service_text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.service_title
+
+
+class ServiceItems(models.Model):
+    author = models.ForeignKey('auth.User')
+    service_item_text = models.CharField(max_length=200)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    service_main = models.ForeignKey(ServicesMain)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+       #list_display =(self.service_item_text, self.service_main.service_title,)
+        return '{0}|| {1}'.format(self.service_item_text, self.service_main.service_title)
+
+       # return list_display
+        #' {0} | состоит в категории | {1}'.format(self.service_item_text, self.service_main.service_title)
--- main_page/views.py
+++ main_page/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import ServicesMain, ServiceItems
 
 # Create your views here.
 
 
 def post_list(request):
-    return render(request, 'main_page/index.html', {})
+    categ_service = ServicesMain.objects.all().order_by('service_title')
+    service_items = ServiceItems.objects.all().order_by('service_main')
+    return render(request, 'main_page/index.html', {"category_service": categ_service, "items": service_items})
